8ef4c165fefdc9244ed7bb214b79c5b6fe4a5563
helloMadhu0922 <littleorbitworld@gmail.com>
2026-08-17 13:55:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function loadProducts() {
 
     try {
 
-        const response = await fetch("products.json?v=6");
+        const response = await fetch("products.json?v=7");
 
         allProducts = await response.json();
 
