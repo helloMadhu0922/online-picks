fdd1cb6aced68b6e29cc62a649f3dba204c71954
helloMadhu0922 <littleorbitworld@gmail.com>
2026-09-26 13:23:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ async function loadProducts() {
     try {
 
         const response =
-            await fetch("products.json?v=39");
+            await fetch("products.json?v=40");
 
         if (!response.ok) {
             throw new Error("Unable to load products.json");
